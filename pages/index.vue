392cb7067b75b3a067599be988cbc27e143dca8b
k2 sl <kiyanosh.s@gmail.com>
2021-10-03 20:12:27 +0330

    the Beginning

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+<div>
+  <Hero/>
+  <LargeCardDisply
+  v-for="cardInfo in largeCardInfo"
+  :key="cardInfo.id"
+  :cardsSetion="cardInfo"
+  />
+</div>
+  
+</template>
+
+<script lang="ts">
+import { largeCardSections, smallCardSections } from "../assets/data.js"
+export default {
+  data() {
+    return {
+      largeCardInfo: largeCardSections,
+      smallCardInfo: smallCardSections
+    }
+  }
+
+}
+
+</script>
